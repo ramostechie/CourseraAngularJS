c31547eb8e8ff6088702715881995fdcb6b323d4
ramostechie <jose.ramos@ramostechie.com>
2016-10-30 11:22:08 +0100

    Assignment 1

---
+++ module1-solution/js/app.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+
+    .controller('LunchCheckController', function ($scope) {
+        $scope.items = "";
+        $scope.itemsMessage = "";
+        $scope.textboxStyle = "";
+        $scope.messageStyle = "";
+
+
+        $scope.displayMessage = function () {
+            $scope.itemsMessage = buildMessage($scope.items);
+        };
+
+        function buildMessage(string) {
+            var totalItemsRaw = string.split(',');
+            var totalItems = 0;
+
+            for (var i = 0; i < totalItemsRaw.length; i++) {
+                if (!isEmptyEl(totalItemsRaw,i))
+                    totalItems += 1;
+            }
+
+            if (totalItems < 1) {
+                $scope.textboxStyle = "border:1px solid red;";
+                $scope.messageStyle = "color:red;";
+                return "Please enter data first";
+            }
+
+            $scope.textboxStyle = "border:1px solid green;";
+            $scope.messageStyle = "color:green;";
+            if (totalItems < 4)
+                return "Enjoy!";
+            else
+                return "Too much!";
+        }
+
+        function isEmptyEl(array, i) {
+            return !(array[i]);
+        }
+    });
+
+})();
