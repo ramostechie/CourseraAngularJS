4e9bbca08bb950c06c67150252e0f833837d288b
ramostechie <jose.ramos@ramostechie.com>
2016-11-08 15:40:12 +0100

    Assignment 3

---
+++ module3-solution/js/app.js
@@ -0,0 +1,127 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItemsDirective)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+    function FoundItemsDirective() {
+        var ddo = {
+            restrict: "E",
+            templateUrl: 'templates/foundItems.html',
+            scope: {
+                foundItems: '<',
+                foundMsg: '<',
+                onRemove: '&'
+            },
+            //controller: NarrowItDownControllerDirective,
+            //controllerAs: 'list',
+            //bindToController: true
+        };
+
+        return ddo;
+    }
+
+    function NarrowItDownControllerDirective() {
+    }
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var menu = this;
+
+        menu.searchTerm = "";
+        menu.found = [];
+        menu.foundMessage = "-1";
+
+        menu.btnNarrowItDown = function () {
+
+            menu.found = [];
+            menu.foundMessage = "-1";
+
+            if (menu.searchTerm.length > 0) {
+                var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+
+                promise.then(function (response) {
+                    menu.found = response;
+                    if (menu.found.length < 1)
+                        menu.foundMessage = "Nothing found";
+                })
+                .catch(function (error) {
+                    menu.foundMessage = "Nothing found";
+                })
+            }
+            else {
+                menu.foundMessage = "Nothing found";
+            }
+        };
+
+        menu.removeItem = function (itemIndex) {
+            menu.found.splice(itemIndex, 1);
+            if (menu.found.length < 1)
+                menu.foundMessage = "All";
+
+        };
+
+        //var promise = MenuSearchService.getMenuCategories();
+
+        //promise.then(function (response) {
+        //    menu.categories = response.data;
+        //})
+        //.catch(function (error) {
+        //    console.log("Something went terribly wrong.");
+        //});
+
+    }
+
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        //service.getMenuCategories = function () {
+        //    var response = $http({
+        //        method: "GET",
+        //        url: (ApiBasePath + "/categories.json")
+        //    });
+
+        //    return response;
+        //};
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json")
+            }).then(function (result) {
+                // process result and only keep items that match
+                var allItems = result.data.menu_items;
+                var foundItems = [];
+                for (var i = 0; i < allItems.length; i++) {
+                    var thisObject = allItems[i];
+                    if (thisObject.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+                        foundItems.push(thisObject);
+                    }
+                }
+                // return processed items
+                return foundItems;
+            });
+
+            return response;
+        };
+
+        //service.getMenuForCategory = function (shortName) {
+        //    var response = $http({
+        //        method: "GET",
+        //        url: (ApiBasePath + "/menu_items.json"),
+        //        params: {
+        //            category: shortName
+        //        }
+        //    });
+
+        //    return response;
+        //};
+
+    }
+
+})();
